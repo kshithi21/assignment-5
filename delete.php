30d33330beba034f0cc69049e8a321f21e9b9a64
kshithi21 <85821102+kshithi21@users.noreply.github.com>
2021-06-29 23:49:05 +0530

    Add files via upload

---
+++ delete.php
@@ -0,0 +1,21 @@
+<html>
+
+<body >
+
+        <form method="post" action="del.php">
+    <table border="1">
+        <tr>
+            <th colspan="3" style="text-align: center;"> Delete</th>
+        </tr>   
+
+        <tr>
+            <th>Enter Passport Number</th><th><input type="text" name="passno"></th>
+        </tr>
+        <tr>
+            <th colspan="3" style="text-align:center"><input type="submit" name="submit" value="Delete"></th>
+        </tr>
+    </table>
+</form>
+
+</body>
+</html>
